4c11c81f8cecf84fa0e6de155b8f9f58418996a7
fabioo.junioor <fj_011@hotmail.com>
2024-05-08 21:19:32 -0300

    feat: delete bootstrap-vue and bootstrap

--- src/main.js
+++ src/main.js
@@ -4,11 +4,6 @@ import App from './App.vue'
 // Vue router
 import router from './config/router/router.js'
 
-// Bootstrap-vue
-//import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-//import 'bootstrap/dist/css/bootstrap.css'
-//import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 // Quasar
 import { Quasar, Notify } from 'quasar'
 import '@quasar/extras/material-icons/material-icons.css'
