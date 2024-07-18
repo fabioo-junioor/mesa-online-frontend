04389c9600c5cb939f55e277185a42974989e837
fabioo.junioor <fj_011@hotmail.com>
2024-07-18 02:23:06 -0300

    feat: create new exports format

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Alert from './Alert.vue';
+import NavBar from './NavBar.vue';
+
+import FormUser from './forms/FormUser.vue';
+import FormEditUser from './forms/FormEditUser.vue';
+import FormEstablishment from './forms/FormEstablishment.vue';
+
+
+export {
+    Alert,
+    NavBar,
+    FormUser,
+    FormEditUser,
+    FormEstablishment
+}
---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import Inicio from './Inicio.vue';
+
+import CadastroUsuario from './usuario/CadastroUsuario.vue';
+import EditarSenhaUsuario from './usuario/EditarSenhaUsuario.vue';
+import EditarUsuario from './usuario/EditarUsuario.vue';
+import HomeUsuario from './usuario/HomeUsuario.vue';
+import LoginUsuario from './usuario/LoginUsuario.vue';
+import BuscarEstabelecimento from './usuario/BuscarEstabelecimento.vue';
+
+import CadastroEstabelecimento from './estabelecimento/CadastroEstabelecimento.vue';
+import HomeEstabelecimento from './estabelecimento/HomeEstabelecimento.vue';
+import LoginEstabelecimento from './estabelecimento/LoginEstabelecimento.vue';
+
+export {
+    Inicio,
+    CadastroUsuario,
+    EditarSenhaUsuario,
+    EditarUsuario,
+    HomeUsuario,
+    LoginUsuario,
+    BuscarEstabelecimento,
+    CadastroEstabelecimento,
+    HomeEstabelecimento,
+    LoginEstabelecimento
+}
