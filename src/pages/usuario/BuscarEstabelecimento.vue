27bcad052524fda719e5eaa366912f9eecdb3237
fabioo-junioor <fj_011@hotmail.com>
2024-07-18 11:16:58 -0300

    feat: create new components

--- src/pages/usuario/BuscarEstabelecimento.vue
+++ src/pages/usuario/BuscarEstabelecimento.vue
@@ -1,16 +1,50 @@
 <script setup>
+import { reactive } from 'vue';
+import { CardEstablishment } from '../../components';
+
+const dataCardEstablishment = reactive({
+    name: 'Rock',
+    description: 'descriçao',
+    isOpen: true,
+    isVacancies: true
+});
 </script>
 <template>
-    <div id="buscar-estabelecimento">
-        <h4>buscar estabelecimento</h4>
+    <div id="search-establishment">
+        <h4 class="q-mt-sm q-mb-sm">Estabelecimentos</h4>
+        <div class="search-filters q-mt-sm q-mb-sm">
+
+        </div>
+        <div class="cards-establishment">
+            <CardEstablishment
+                :name="dataCardEstablishment.name"
+                :description="dataCardEstablishment.description"
+                :isVacancies="dataCardEstablishment.isVacancies"
+                :isOpen="dataCardEstablishment.isOpen"
+                v-for="i in 10" :key="i" />
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    #buscar-estabelecimento{
-        display: flex;
-        min-height: calc(100vh - 3.1rem);
-        padding: 1rem;
-        background-color: blueviolet;
+#search-establishment{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: calc(100vh - 3.1rem);
+    padding: 1rem;
+
+    .search-filters{
+        height: 2rem;
+        width: 100%;
+        background-color: red;
 
     }
+    .cards-establishment{
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+        gap: .2rem;
+    }
+
+}
 </style>
--- src/pages/usuario/EditarSenhaUsuario.vue
+++ src/pages/usuario/EditarSenhaUsuario.vue
@@ -1,10 +1,64 @@
 <script setup>
+import { reactive } from 'vue';
+import { FormEditPasswordUser } from '../../components';
 
+const dataFormEditPasswordUser = reactive({
+    oldPassword: '',
+    newPassword: '',
+    repeatNewPassword: ''
+
+});
+const saveFormUser = () => {
+    console.log('edição salva ', dataFormEditPasswordUser);
+
+};
 </script>
 <template>
-
+    <div id="edit-password-user">
+        <h4>Editar Usuário</h4>
+        <FormEditPasswordUser
+            class="form-edit-password-user q-pt-xl q-pb-xl"
+            v-model:oldPassword='dataFormEditPasswordUser.oldPassword'
+            v-model:newPassword='dataFormEditPasswordUser.newPassword'
+            v-model:repeatNewPassword="dataFormEditPasswordUser.repeatNewPassword"
+            @saveFormUser='saveFormUser' />
+    </div>
 </template>
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap");
+#edit-password-user{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 3.1rem);
+    font-family: "Fredoka", sans-serif;
 
+    h4{
+        width: 60%;
+        text-align: center;
+        padding: 1rem;
+        background-color: white;
+        box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, .2);
+        border: 1px solid rgba(0, 0, 0, .2);
+        border-radius: 5px;
+
+    }
+    .form-edit-password-user{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 60%;
+        background-color: white;
+        box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, .2);
+        border: 1px solid rgba(0, 0, 0, .2);
+        border-radius: 5px;
+
+        .q-form{
+            width: 100%;
+            
+        }
+    }
+}
 </style>
