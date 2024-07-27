94a429c1bf1e166dfe24eadba22f0c70506ec659
fabioo.junioor <fj_011@hotmail.com>
2024-07-26 23:01:13 -0300

    feat: create 'formReserveEstablishment' to reserve

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import FormUser from './forms/FormUser.vue';
 import FormEditUser from './forms/FormEditUser.vue';
 import FormEditPasswordUser from './forms/FormEditPasswordUser.vue';
 import FormEstablishment from './forms/FormEstablishment.vue';
+import FormReserveEstablishment from './forms/FormReserveEstablishment.vue';
 
 import CardEstablishment from './cards/CardEstablishment.vue';
 import CardImageProfile from './cards/CardImageProfile.vue';
@@ -17,5 +18,6 @@ export {
     FormEditPasswordUser,
     CardEstablishment,
     FormEstablishment,
-    CardImageProfile
+    CardImageProfile,
+    FormReserveEstablishment
 }
